5a3561baf1447b704218988701e76bab4dfe0142
OOPS_ITBIN-2211-0127 <shresthasanam288@gmail.com>
2024-11-17 08:28:52 +0530

    dsfas

--- utils/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error{
-    constructor(statuscode = 500, message = "Something went wrong"  ) {
-        super();
-        this.message = message
-        this.statuscode = statuscode;
-    }
-}
-
-module.exports = ExpressError;
